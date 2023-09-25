17b72b57c153b2fe4ead9300b159729d13397e66
ookm19 <145653920+ookm19@users.noreply.github.com>
2023-09-25 16:57:22 +0700

    Add files via upload

---
+++ input,out.py
@@ -0,0 +1,11 @@
+#mengambil input
+sisi_string= input("masukin sisi:")
+
+#konversi string ke float,integer
+sisi= int(sisi_string)
+
+#proses
+Luas=sisi*sisi
+
+#menampilkan output
+print ("Luas persegi dari",sisi,"adalah",Luas)
